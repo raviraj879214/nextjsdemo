ce1981d134dca6aaead31c8ff04fcfc47d835ea1
raviraj879214 <ravirajalphainfo@gmail.com>
2025-04-30 15:29:06 +0530

    reversed

--- app/Component/Header.jsx
+++ app/Component/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
           <div className="flex justify-between items-center h-16">
             <div className="flex-shrink-0">
               <a href="/" className="text-xl font-bold text-blue-600">
-                Your logo
+                MyLogo
               </a>
             </div>
 
